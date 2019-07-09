29f64bbc127840c2dd5bcd3f7ac4c49e3211ae71
Ganesh Gore <goreganesh007@gmail.com>
2019-07-09 11:37:17 -0600

    Added installation and cadence setup files

---
+++ cdslib/setup/setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# TIGFET10nm Open Source Predictive Process Design Kit
+# Copyright (c) [2019] [Laboratory for NanoIntegrated Systems]
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+###############################################################################
+
+# Set the PDK_DIR variable to the root directory of the project
+export PDK_DIR=../
+
+if [ ! -f ${PWD}/.cdsinit ]; then
+  cp ${PDK_DIR}/cdslib/setup/cdsinit ${PWD}/.cdsinit
+fi
+
+if [ ! -f ${PWD}/cds.lib ]; then
+  cp ${PDK_DIR}/cdslib/setup/cds.lib ${PWD}/cds.lib
+fi
+
+if [ ! -f ${PWD}/.runset.calibre.drc ]; then
+  cp ${PDK_DIR}/cdslib/setup/runset.calibre.drc ${PWD}/.runset.calibre.drc
+fi
+
+if [ ! -f ${PWD}/.runset.calibre.lvs ]; then
+  cp ${PDK_DIR}/cdslib/setup/runset.calibre.lvs ${PWD}/.runset.calibre.lvs
+fi
+
+if [ ! -f ${PWD}/.runset.calibre.pex ]; then
+  cp ${PDK_DIR}/cdslib/setup/runset.calibre.pex ${PWD}/.runset.calibre.pex
+fi
+
+export MGC_CALIBRE_DRC_RUNSET_FILE=./.runset.calibre.drc
+export MGC_CALIBRE_LVS_RUNSET_FILE=./.runset.calibre.lvs
+export MGC_CALIBRE_PEX_RUNSET_FILE=./.runset.calibre.pex
